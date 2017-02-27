6da707b5ba8dac8064e53583753f0986129b12b5
Ravon L Wright <flyr397@gmail.com>
2017-02-27 09:08:23 -0500

    added
    added program

--- squareApplication/Program.cs
+++ squareApplication/Program.cs
@@ -1,15 +1,49 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
+
 
 namespace squareApplication
 {
-    class Program
+    class Square
+    {
+        
+        double sideA;
+        double sideB;
+        //string root = sideA * SideB;
+
+        public void AcceptDetails()
+        {
+            sideA = 16.5;
+            sideB = 16.5;
+        }
+
+        public double GetArea()
+        {
+            return sideA * sideB;
+        }
+
+        public void Display()
+        {
+            WriteLine("Side : {0}", sideA);
+            WriteLine("Side : {0}", sideB);
+            WriteLine("Area : {0}", GetArea());
+            ReadKey();
+        }
+    }
+
+    class ExecuteSquare
     {
         static void Main(string[] args)
         {
+            Square s = new Square();
+
+            s.AcceptDetails();
+            s.Display();
+            ReadLine();
         }
     }
 }
